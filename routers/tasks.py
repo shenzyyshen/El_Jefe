ff8c734ad0c873938b84df2153cab960ba26a5fa
Ayelen may <Ayelen.kirchhoff@gmail.com>
2025-11-20 16:02:44 +0100

    goal_manager working, task in dashboard

--- routers/tasks.py
+++ routers/tasks.py
@@ -1,8 +1,9 @@
 """ tasks.py manages task updates (edit and complete """
 
-from fastapi import APIRouter, Depends, Request, Body, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from core import database, schemas
+from core.database import SessionLocal, Base, engine
+from core import schemas
 from routers import models
 
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
@@ -10,7 +11,7 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 """Dependency function that creates a DB session for each request."""
 def get_db():
-    db = database.SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -33,18 +34,22 @@ def update_task(
 def complete_task(task_id: int, db: Session = Depends(get_db)):
     task =db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
-        raise HTTPException(status_code=404, detail="Task not found")
-    task.completed = True
+        return {"error": "Task not found"}
+    task.completed = not task.completed
     db.commit()
-    db.refresh(task)
-    return task
+    return {"task_id": task.id, "completed": task.completed}
 
 @router.patch("/tasks/{task_id}/complete_toggle")
-def toggle_task(task_id: int, completed: bool = Body(...), db: Session=Depends(get_db)):
-    task = db.query(models.Task).get(task_id)
-    task.completed =completed
+def toggle_task_complete(task_id: int, db: Session=Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == task_id).first()
+    if not task:
+        return {"error": "Task not found"}
+
+    #toggle completion
+    task.completed = not task.completed
     db.commit()
-    return {"status": "ok"}
+    db.refresh(task)
+    return {"task_id": task.id, "completed": task.completed}
 
 
 
